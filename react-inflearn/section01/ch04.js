a9b60ec1d2c73d4771305d42af8540ca73adf71c
ssongjiyeon <b8480@naver.com>
2024-08-12 06:20:46 +0900

    인프런 강의 코드 학습
    - 출처 : 한입 크기로 잘라먹는 리액트[https://www.inflearn.com/course/%ED%95%9C%EC%9E%85-%EB%A6%AC%EC%95%A1%ED%8A%B8]

---
+++ react-inflearn/section01/ch04.js
@@ -0,0 +1,33 @@
+// 변수
+let age = 29;
+age = 30
+
+// 상수
+const birth = "22"
+
+// 자료형
+// number type - 사칙연산 모두 지원(모듈러 연산)
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity
+let mInf = -Infinity
+
+let nan = NaN // 난 not a number 사칙연산 실패 시 나옴
+
+// String
+let myName = "송";
+let lastName = "지";
+let name = myName + lastName
+
+let text = `${name}`
+
+// boolean
+let isTrue = true
+
+// null
+let empty = null;
+
+// undefined type - 아무런 값도 안넣은 것
+let none;
