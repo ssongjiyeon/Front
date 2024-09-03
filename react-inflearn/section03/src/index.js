3a84dc2e325f6d189f2459790cd48a7ae1d3b14c
ssongjiyeon <b8480@naver.com>
2024-09-04 07:09:21 +0900

    [[inflearn] ES 모듈 사용 방법

--- react-inflearn/section03/src/index.js
+++ react-inflearn/section03/src/index.js
@@ -1,3 +1,10 @@
-const moduleData = require("./math");
-console.log(moduleData.add(1, 2));
-console.log(moduleData.sub(1, 2));
+// const moduleData = require("./math");
+// console.log(moduleData.add(1, 2));
+// console.log(moduleData.sub(1, 2));
+
+// ES 모듈 시스템을 사용할 때는 파일의 확장자까지 작성해야 한다.
+import doubleN, { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(doubleN(2, 3));
--- react-inflearn/section03/src/math.js
+++ react-inflearn/section03/src/math.js
@@ -8,7 +8,12 @@ function sub(a, b) {
   return a - b;
 }
 
-module.exports = {
-  add,
-  sub,
-};
+export function doubleN(a, b) {
+  return a * b;
+}
+
+// module.exports = {
+//   add,
+//   sub,
+// };
+export { add, sub };
